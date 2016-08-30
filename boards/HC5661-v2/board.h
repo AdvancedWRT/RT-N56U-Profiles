5f57c2617187f004b1e8432fbfbfd7600c016e92
Demetris <ierokipides.dem@gmail.com>
2016-08-30 18:05:33 +0300

    Add Eylike-SS aka HC5661 clone support to padavan/rt-n65u firmware Note: Internet lan activity led gpio is missing for now Wifi+Power Leds work

---
+++ boards/HC5661-v2/board.h
@@ -0,0 +1,29 @@
+/* Eylike SS aka HC5661 clone */
+
+#define BOARD_PID		"HC5661-v2"
+#define BOARD_NAME		"HC5661-v2"
+#define BOARD_DESC		"Eylike SS aka HC5661 clone"
+#define BOARD_VENDOR_NAME	"Eylike"
+#define BOARD_VENDOR_URL	"https://Eylike.com/"
+#define BOARD_MODEL_URL		"https://Eylike.com/HC5661-v2"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	90
+#define BOARD_GPIO_BTN_RESET	12
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	72
+#define  BOARD_GPIO_LED_POWER 9
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
