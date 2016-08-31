ec6744d9aec39c1540c3c54b8b0bf5d4af68c0c7
Demetris <ierokipides.dem@gmail.com>
2016-08-31 21:49:11 +0300

    Add MQmaker WiTi and seperate configs

---
+++ MQmaker-WiTi/boards/board.h
@@ -0,0 +1,35 @@
+/* MQmaker WiTi */
+
+#define BOARD_PID		"WITI"
+#define BOARD_NAME		"WITI"
+#define BOARD_DESC		"Mqmaker WiTi Wireless Router"
+#define BOARD_VENDOR_NAME	"MQmaker"
+#define BOARD_VENDOR_URL	"https://mqmaker.com/"
+#define BOARD_MODEL_URL		"https://mqmaker.com/doc/WiTi/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	40
+#define BOARD_GPIO_BTN_RESET	18
+#define BOARD_GPIO_BTN_WPS	17
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef BOARD_GPIO_LED_POWER
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_USB2
+#undef  BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB_ON
+#undef BOARD_GPIO_PWR_USB
+#undef BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	8
+#undef BOARD_ETH_LED_SWAP
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	1	/* USB3.0 = #2, USB2.0 = #1 */
